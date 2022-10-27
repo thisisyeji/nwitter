217c6505e3a5413473e430f87d7d62f09e53d491
yjkim0109 <yjkkim0109@gmail.com>
2022-10-27 15:36:40 +0900

    Feat: 사진 추가, 삭제 기능

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { dbService } from 'fbase';
 import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { deleteObject, ref } from 'firebase/storage';
+import { storageService } from 'fbase';
 
 const Nweet = ({ nweetObj, isOwner }) => {
 	const [editing, setEditing] = useState(false);
@@ -11,6 +13,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
 		const ok = window.confirm('Are you sure you want to delete this nweet?');
 		if (ok) {
 			await deleteDoc(NweetTextRef);
+			if (nweetObj.attachmentUrl) {
+				await deleteObject(ref(storageService, nweetObj.attachmentUrl));
+			}
 		}
 	};
 	const toggleEditing = () => setEditing((prev) => !prev);
@@ -47,6 +52,9 @@ const Nweet = ({ nweetObj, isOwner }) => {
 			) : (
 				<>
 					<h4>{nweetObj.text}</h4>
+					{nweetObj.attachmentUrl && (
+						<img src={nweetObj.attachmentUrl} width='50px' height='50px' />
+					)}
 					{isOwner && (
 						<>
 							<button onClick={onDeleteClick}>Delete Nweet</button>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { dbService } from 'fbase';
+import React, { useState, useEffect, useRef } from 'react';
+import { dbService, storageService } from 'fbase';
 import {
 	collection,
 	addDoc,
@@ -7,11 +7,15 @@ import {
 	query,
 	orderBy,
 } from 'firebase/firestore';
+import { ref, uploadString, getDownloadURL } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
 import Nweet from 'components/Nweet';
 
 const Home = ({ userObj }) => {
 	const [nweet, setNweet] = useState('');
 	const [nweets, setNweets] = useState([]);
+	const [attachment, setAttachment] = useState('');
+	const fileInput = useRef();
 
 	/*
 	// 데이터 불러오기 - 이전 데이터를 가져오는 방식
@@ -42,19 +46,52 @@ const Home = ({ userObj }) => {
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
-		// 데이터 추가
-		await addDoc(collection(dbService, 'nweets'), {
+		let attachmentUrl = '';
+		if (attachment !== '') {
+			const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+			const response = await uploadString(
+				attachmentRef,
+				attachment,
+				'data_url'
+			);
+			attachmentUrl = await getDownloadURL(response.ref);
+		}
+		const nweetObj = {
 			text: nweet,
 			createdAt: Date.now(),
 			creatorId: userObj.uid,
-		});
+			attachmentUrl,
+		};
+		// 데이터 추가
+		await addDoc(collection(dbService, 'nweets'), nweetObj);
 		setNweet('');
+		setAttachment('');
 	};
 	const onChange = (e) => {
 		const {
 			target: { value },
 		} = e;
 		setNweet(value);
+	};
+
+	const onFileChange = (e) => {
+		const {
+			target: { files },
+		} = e;
+		const theFile = files[0];
+		const reader = new FileReader();
+		reader.onloadend = (finishedEvent) => {
+			const {
+				currentTarget: { result },
+			} = finishedEvent;
+			setAttachment(result);
+		};
+		reader.readAsDataURL(theFile);
+	};
+
+	const onClearAttachment = () => {
+		setAttachment('');
+		fileInput.current.value = '';
 	};
 
 	return (
@@ -67,7 +104,19 @@ const Home = ({ userObj }) => {
 					value={nweet}
 					onChange={onChange}
 				/>
+				<input
+					type='file'
+					accept='image/*'
+					onChange={onFileChange}
+					ref={fileInput}
+				/>
 				<input type='submit' value='Nweet' />
+				{attachment && (
+					<div>
+						<img src={attachment} width='50px' height='50px' />
+						<button onClick={onClearAttachment}>Clear</button>
+					</div>
+				)}
 			</form>
 			<div>
 				{nweets.map((nweet) => (
